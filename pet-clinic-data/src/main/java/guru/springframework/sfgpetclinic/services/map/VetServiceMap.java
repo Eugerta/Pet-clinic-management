f3b160fb645cffd0708c529cbde088f0c89183cc
DESKTOP-J7B3VA0\Klevin <klevindelimeta@hotmail.com>
2020-12-16 09:22:10 +0100

    Fix broken links and refactoring VetServiceMap

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,15 +1,30 @@
 package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.Speciality;
 import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.SpecialityService;
 import guru.springframework.sfgpetclinic.services.VetService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
-    @Override
+    
+	
+	
+	@Autowired
+	private  SpecialityService specialityService;
+	
+	
+
+
+	@Override
     public Set<Vet> findAll() {
         return super.findAll();
     }
@@ -26,6 +41,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+    	
+    	if(object.getSpeciality().size()>0) {
+    		object.getSpeciality().forEach(speciality-> {
+    			if(speciality.getId()==null) {
+    				Speciality savedSpeciality = specialityService.save(speciality);
+    			}
+    		}
+    		);
+    	}
         return super.save(object);
     }
 
